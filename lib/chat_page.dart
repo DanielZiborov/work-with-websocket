7ab77ddd2e3a762e75c54e916c22630b0273af6e
DanielZiborov <ziborovdanila5@gmail.com>
2025-01-11 21:12:40 +0300

    commit 1

---
+++ lib/chat_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Chat(
+        messages: const [],
+        onSendPressed: (val) {},
+        user: const types.User(id: ''),
+      ),
+    );
+  }
+}
