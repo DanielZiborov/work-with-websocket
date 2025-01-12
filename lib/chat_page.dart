3bc6306e4009a0ba99de14902b2a66997b03c313
DanielZiborov <ziborovdanila5@gmail.com>
2025-01-12 12:36:01 +0300

    commit2

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -10,12 +11,51 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final channel = WebSocketChannel.connect(
+    Uri.parse(
+      'wss://echo.websocket.org',
+    ),
+  );
+
+  Future<void> chatWs() async {
+    await channel.ready;
+    channel.stream.listen(
+      (message) {
+        print(message);
+      },
+    );
+  }
+
+  void addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  List<types.Message> _messages = [];
+
+  @override
+  void initState() {
+    chatWs();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Chat(
-        messages: const [],
-        onSendPressed: (val) {},
+        messages: _messages,
+        onSendPressed: (val) {
+          addMessage(
+            const types.TextMessage(
+              author: types.User(
+                id: '',
+              ),
+              id: '',
+              text: 'data',
+            ),
+          );
+        },
         user: const types.User(id: ''),
       ),
     );
