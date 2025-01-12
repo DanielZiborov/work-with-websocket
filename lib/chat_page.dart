32a2e6f27d131806ebae77923d36ae63f5ad6e79
DanielZiborov <ziborovdanila5@gmail.com>
2025-01-12 18:42:20 +0300

    final

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -5,60 +5,55 @@ import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-class ChatPage extends StatefulWidget {
+class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
-
-  @override
-  State<ChatPage> createState() => _ChatPageState();
-}
-
-class _ChatPageState extends State<ChatPage> {
-  final channel = WebSocketChannel.connect(
-    Uri.parse(
-      'wss://echo.websocket.org',
-    ),
-  );
-
-  final _user = const types.User(
-    id: '1',
-  );
-
-  final _channel = const types.User(
-    id: '2',
-  );
-
-  List<types.Message> _messages = [];
-
+  
   @override
   Widget build(BuildContext context) {
+    final channel = WebSocketChannel.connect(
+      Uri.parse(
+        'wss://echo.websocket.org',
+      ),
+    );
+
+    const user = types.User(
+      id: '1',
+    );
+
+    const webSocket = types.User(
+      id: '2',
+    );
+
+    List<types.Message> messages = [];
+
     return Scaffold(
       body: StreamBuilder<dynamic>(
         stream: channel.stream,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.active) {
-            _messages.insert(
+            messages.insert(
               0,
               types.TextMessage(
-                author: _channel,
+                author: webSocket,
                 id: Random().nextInt(10000).toString(),
                 text: snapshot.data.toString(),
               ),
             );
             return Chat(
               showUserAvatars: true,
-              messages: _messages,
+              messages: messages,
               onSendPressed: (val) {
                 channel.sink.add(val.text);
-                _messages.insert(
+                messages.insert(
                   0,
                   types.TextMessage(
-                    author: _user,
+                    author: user,
                     id: Random().nextInt(10000).toString(),
                     text: val.text,
                   ),
                 );
               },
-              user: _user,
+              user: user,
             );
           }
           return const Center(
